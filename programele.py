825dd39099367f7567847621ed7169d043d2a550
arnaszs <bolisas8@gmail.com>
2023-04-13 09:54:53 +0300

    zaidimukas atspėk skaiciu, pakeisti emoji

--- programele.py
+++ programele.py
@@ -4,14 +4,14 @@ def spejimas():
     num_spejimas = 0
     
     while True:
-        spejamas_skaicius = int(input("atspėk numerį nuo 1 - 100: "))
+        spejamas_skaicius = int(input("Atspėk numerį nuo 1 - 100: "))
         num_spejimas += 1
         
         if spejamas_skaicius == slaptas_skaicius:
-            print("Sveikinu! Tu atspėjai numerį iš", num_spejimas, "bandymų!")
+            print("\U00002B50 \U00002705 Sveikinu! Tu atspėjai numerį iš", num_spejimas, "bandymų! \U00002705 \U00002B50")
             break
         elif spejamas_skaicius < slaptas_skaicius:
-            print("Skaičius yra per žemas, spėk dar kartą!")
+            print("\U0000274C Skaičius yra per žemas, spėk dar kartą! \U0000274C")
         else:
-            print("Skaičius yra per aukštas, spėk dar kartą!")
+            print("\U0000274C Skaičius yra per aukštas, spėk dar kartą! \U0000274C")
 spejimas()
