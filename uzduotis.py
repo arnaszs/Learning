88cbd0e372e66aae22162e26ca482a8786de2503
arnaszs <bolisas8@gmail.com>
2023-04-24 15:17:55 +0300

    pvd

--- uzduotis.py
+++
@@ -1,4 +0,0 @@
-atlygis = input("Iveskite savo atlygi: ")
-procentas = input("Iveskite savo mokesciu procenta: ")
-neto = int(atlygis) * (1 - int(procentas) / 100)
-print(f"Jusu atlyginimas i rankas yra {neto:.x2f} EUR")
